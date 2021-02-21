0ab49fdbd5049ae20d016130e00c8e39883bb622
Singh, Atin <>
2021-02-21 13:23:17 -0500

    updated jenkins file

---
+++ Jenkinfile.sh
@@ -0,0 +1,42 @@
+pipeline {
+    agent any
+    tools {
+        maven 'maven3'
+        jdk 'jdk8'
+    }
+
+
+    stages{
+        stage('Preparattion') {
+            steps {
+                sh 'echo hello world'
+            }
+        }
+        stage('CheckOut SCM') {
+            steps {
+                git 'https://github.com/atinsingh/devopsproject.git'
+            }
+        }
+        stage('Compile & Package') {
+            steps {
+                sh 'mvn package -DskipTests'
+            }
+        }
+        stage('Build Image') {
+            steps {
+                sh "docker image build . -t pragra/api"
+            }
+        }
+        stage('Publish Image') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) {
+                  sh "docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}"
+                  sh "docker image push pragra/api"
+                }
+            }
+        }
+
+    }
+
+
+}
